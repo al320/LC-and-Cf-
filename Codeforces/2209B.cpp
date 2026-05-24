f361271af871827ba59b790c53f47a545a6bb09b
al320 <alokdhruw@888gmail.com>
2026-05-24 22:29:46 +0530

    Array Problem 2209B

--- Codeforces/2209B.cpp
+++ Codeforces/2209B.cpp
@@ -33,3 +33,5 @@ int main(){
 
     return 0;
 }
+
+//The approach: Calculate the max(numbers greater than the number at index i,numbers lesser than the number at index i) and print out them with a complexity of O(n^2)
