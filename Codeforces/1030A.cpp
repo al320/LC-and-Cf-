62137e36dfb4be1981d595825da63b4b0f34cdd4
al320 <alokdhruw888@gmail.com>
2026-05-29 13:48:20 +0530

    In Search of an Easy Problem

---
+++ Codeforces/1030A.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n;
+    cin>>n;
+    while(n--){
+        int a;
+        cin>>a;
+        if(a == 1){
+            cout<<"HARD"<<'\n';
+            return;
+        }
+    }cout<<"EASY"<<'\n';
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    solve();
+    return 0;
+}
+
